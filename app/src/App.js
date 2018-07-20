76fea75e650ceec5be0b89cdc4c03bb05423dd06
koseisakashita <s09072302394@gmail.com>
2018-07-21 03:14:15 +0900

    create function Component

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={() => {alert(535353)}}/>
-      </React.Fragment>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onClick={() => {alert(535353)}}/>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <p>Meow!</p>
 }
 
 export default App;
